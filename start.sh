62968ff6901f2bf14be095f239a014aea94fcd09
Apollo Tang <tang.apollo@gmail.com>
2018-10-06 01:37:59 -0400

    edit src location

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # browser-sync start --server 'src' --files 'src' -c 'bs-config.js' --directory &
 
-# filesToWatch='/Users/apollotang/3/g-school/g-eh/eh-convert-scss-sass-to-css-in-js/code/codes-notes/11-mixed-mode--string-or-obj/'
-# servedDir='/Users/apollotang/3/g-school/g-eh/eh-convert-scss-sass-to-css-in-js/code/codes-notes/11-mixed-mode--string-or-obj/'
+filesToWatch='/Users/apollotang/0/g-labs/src-3040/'
+servedDir='/Users/apollotang/0/g-labs/src-3040/'
 
-filesToWatch='src'
-servedDir='src'
+# filesToWatch='src'
+# servedDir='src'
 
 browser-sync start --server "${servedDir}" --files "${filesToWatch}" -c 'bs-config.js' --directory &
 wait;
