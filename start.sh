b6d624673830d95f692dc4ef276eee145db26c8b
Apollo Tang <tang.apollo@gmail.com>
2018-10-06 00:48:14 -0400

    use configurable serverDir and filesDirToWatch

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-browser-sync start --server 'src' --files 'src' -c 'bs-config.js' --directory &
+# browser-sync start --server 'src' --files 'src' -c 'bs-config.js' --directory &
+
+# filesToWatch='/Users/apollotang/3/g-school/g-eh/eh-convert-scss-sass-to-css-in-js/code/codes-notes/11-mixed-mode--string-or-obj/'
+# servedDir='/Users/apollotang/3/g-school/g-eh/eh-convert-scss-sass-to-css-in-js/code/codes-notes/11-mixed-mode--string-or-obj/'
+
+filesToWatch='src'
+servedDir='src'
+
+browser-sync start --server "${servedDir}" --files "${filesToWatch}" -c 'bs-config.js' --directory &
 wait;
